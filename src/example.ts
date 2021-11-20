cc9d0d71da908f003f13fef6f23cdc21d11bbe0e
Hoshi Ran <ureshinostyle350@gmail.com>
2021-11-21 00:37:54 +0900

    [add] example ファイル

---
+++ src/example.ts
@@ -0,0 +1,19 @@
+import { calculate } from ".";
+
+console.log(calculate(["1", "2", "3", "4"]));
+// -> [ "((1+2)+3)+4", "((1+2)+4)+3", "(3+4)+(1+2)",
+//      ... 51 more items
+//    ]
+
+const input = "1199";
+console.log(calculate(input.split("")));
+// -> ["((1/9)+1)*9"]
+
+console.log(calculate(["2", "5"]));
+// -> ["2*5"]
+
+console.log(calculate(["1", "2", "4"]));
+// -> ["(1+4)*2"]
+
+console.log(calculate(["5", "9", "9", "9", "9"]));
+// -> ["((9/9)+(9/9))*5"]
